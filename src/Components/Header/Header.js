86c222c88c0cf5a2e4897b7dfe9ae37bc786da8f
Ruston Reformado <rustonreformado@gmail.com>
2018-02-09 21:31:49 -0700

    Landing page styling, working countdown to next product

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,20 @@ class Header extends Component {
     render() {
         return (
             <div className='header-container'>
-                This is the header
+
+                <div className='upper-header'>
+                    Test
+                </div>
+
+                <hr />
+
+                <div className="navbar">
+                    <a href="/">Test</a>
+                    <a href="/">Test</a>
+                    <a href="/">Test</a>
+                    <a href="/">Test</a>
+                </div>
+
             </div>
         );
     }
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,44 +1,86 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-import { connect } from 'react-redux'
-import {bindActionCreators} from 'redux'
-import { getHourlyProduct} from '../.././ducks/reducer'
+// import axios from 'axios';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import { getHourlyProduct} from '../.././ducks/reducer';
+import ReactCountdownClock from 'react-countdown-clock';
+
+
+import './Landing.css';
 
 class Landing extends Component {
     constructor(){
         super();
         this.state = {
-            products: []
+            timeUp: false
         }
     }
 
     componentWillMount(){
         this.props.getHourlyProduct()
     }
-    render() {
-        if(!this.props.hourlyProduct){
-            return <div>fuck</div>
-        }
-        console.log(this.props.hourlyProduct)
+    
+    render() { 
 
+        let date = new Date();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let second = date.getSeconds();
+        // console.log(minute, second)
+
+        let secondsRemaining = ((60 - minute) * 60) + (60 - second);
+        // console.log(secondsRemaining)
+
+        //the number below will be changed to variable hour when database is filled
+        let product = this.props.hourlyProduct[hour];
+        if(!product){
+            return <img src='http://datainflow.com/wp-content/uploads/2017/09/loader.gif' />
+        }
+        // console.log(this.state.timeUp)
         return (
-            
             <div>
-                {this.props.hourlyProduct.map(e => {
-                    let date = new Date;
-                    let hour = date.getHours();
-                    console.log(hour)
-                    //the number 2 below will be changed to variable hour when database is filled
-                    if(e.productid == 2){
-                        return e.productname
-                    }
-                })}
+                <div className='todays-woot'>Todays Woot</div>
+
+                <div hidden={!this.state.timeUp}>You missed it!</div>
+
+                <div style={this.state.timeUp ? {display: 'none'} : {} } className='product-container'>
+                    <div className='product-info-container'>
+                        <h1 className='product-info'>Woot: {product.productname}</h1>
+                        <h2 className='product-info'>MSRP: ${product.fullprice}.00</h2>
+                        <h2 className='product-info'>Our Price: ${product.saleprice}.00</h2>
+                        <p className='product-info'>Description: {product.description}</p>
+                        <button className='add-to-cart-button'>I want it!</button>
+
+                        <p className='product-info'>Remaining: {product.quantity}</p>
+                        <div className='product-info'>
+                            <p>Time Remaining: </p>
+                            <div className='countdown-clock'>
+                                <ReactCountdownClock seconds={secondsRemaining}
+                                    color="#000"
+                                    alpha={0.9}
+                                    size={100}
+                                    timeFormat={'hms'}
+                                />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='product-image-container'>
+                        <img alt='product' className='product-image' src={product.imgurl} />
+                    </div>
+                </div>
+
+                <div>
+                    Test Advertisement
+                </div>
+
             </div>
         );
     }
 }
+
 function mapStateToProps(state){
-    console.log(state)
+    // console.log(state)
     return {
         hourlyProduct: state.hourlyProduct
     }
