b3204a89061c3afac9f2663002d2a043efd96744
Ruston Reformado <rustonreformado@gmail.com>
2018-02-10 22:46:10 -0700

    back button in cart component

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Cart.css';
 
 class Cart extends Component {
     render() {
         return (
             <div>
-                Cart
+                <Link to='/'><button className='return-to-landing-button'>Back</button></Link>
             </div>
         );
     }
