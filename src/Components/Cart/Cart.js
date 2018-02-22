a3cc4fe3e0ca2e1b67e968dd93265c071b89314c
Alex Hill <big_al_hill@comcast.net>
2018-02-21 17:11:31 -0700

    Functioning cart, working on implementing stripe

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import ReactCountdownClock from 'react-countdown-clock';
-
+import StripeCheckout from 'react-stripe-checkout';
 import {getTimeRemaining} from '../../Util/Util';
 import {bindActionCreators} from 'redux';
 import {checkoutCart} from '../.././ducks/reducer'
@@ -10,10 +10,6 @@ import {checkoutCart} from '../.././ducks/reducer'
 import './Cart.css';
 
 class Cart extends Component {
-    // getTimeRemaining(){
-    //     console.log('test')
-    // }
-
     render() {
         let cartItems = this.props.cart.map( e => {
             console.log(e)
@@ -22,17 +18,30 @@ class Cart extends Component {
                 <div> {e.productname}  </div>
                 <div>Price: ${e.saleprice} </div>
                 <div> SOLD ON WOOT  </div>
-                <button onClick={() => this.props.checkoutCart(this.props.cart)}> CHECKOUT </button> 
                 
             </div>
 
             )
         });
+
+        let totalCart
+        if(this.props.cart.length > 1){ 
+            totalCart = this.props.cart.map( (e, i, arry) => {
+            return e.saleprice + arry[i + 1].saleprice
+         }) } else if(this.props.cart.length === 1) {
+            totalCart = this.props.cart[0].saleprice
+         } else if(!this.props.cart) { console.log('i am empty bitch')} 
+
         return (
             <div className='main-container-cart'>
                 <div className='content-left-cart'>
                     <h1 className='shopping-cart-header'> Shopping Cart ({this.props.cart.length === 1 ? this.props.cart.length + ' Item' : this.props.cart.length + ' Items' })</h1>
                     <div className='cart-item'>{cartItems}</div>
+                <div onClick={() => this.props.checkoutCart(this.props.cart)}><StripeCheckout
+                    token={token}
+                    stripeKey="pk_test_iK0PyzokdY1afxWvhlU5qnOA"
+                    amount={totalCart * 100}
+                /></div>
                 </div>
                 <div className='cart-countdown-timer' hidden={!this.props.cart.length}>
                     Time remaining to make purchase
